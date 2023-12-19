e1745611c3c1051b2db78e8a6b733eaade4a8643
Ramiro <GARZA.RAMIROV@GMAIL.COM>
2023-12-19 13:46:01 -0600

    Separate player computer logic

---
+++ src/app/views/base.js
@@ -0,0 +1,14 @@
+export const elements = {
+  playerGameboard: document.querySelector(".player-gameboard"),
+  computerGameboard: document.querySelector(".computer-gameboard"),
+  playerGrid: document.querySelector(".player-grid"),
+  computerGrid: document.querySelector(".computer-grid"),
+  infoContainer: document.querySelector(".info-container"),
+  infoText: document.querySelector(".info-text"),
+  autoPlaceBtn: document.querySelector(".fleet-autoplace"),
+  playAgainBtn: document.querySelector(".play-again"),
+  startBtn: document.querySelector(".start"),
+  fleetContainer: document.querySelector(".fleet-container"),
+  fleetDraggable: document.querySelector(".fleet-draggable"),
+  fleetInfo: document.querySelector(".fleet-info"),
+};
